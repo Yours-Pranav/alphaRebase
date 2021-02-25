6a77d4b4b00a88619855f55ef3c45e299955281c
billybot482 <72440832+billybot482@users.noreply.github.com>
2021-02-25 11:59:00 +0800

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -407,7 +407,7 @@ def stats(update: Update, context: CallbackContext):
     stats = "<b>Current stats:</b>\n" + "\n" + output + "\n".join(
         [mod.__stats__() for mod in STATS])
     result = re.sub(r'(\d+)', r'<code>\1</code>', stats)
-    update.effective_message.send_message(result, parse_mode=ParseMode.HTML)
+    bot.send_message(result, parse_mode=ParseMode.HTML)
 
 
 @run_async
