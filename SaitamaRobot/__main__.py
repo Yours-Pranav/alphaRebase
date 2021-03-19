1017417b65124e7af8cf6d74a79a960b82f304bd
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-19 08:47:16 +0530

    Fixes ✅

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -51,9 +51,10 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hi {}, my name is {}! 
-This is BCB group management Bot
-
+Hi *{}*, my name is *{}*! 
+This is BCB group management Botbot.
+You can find my list of available commands with /help.
+"""
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
@@ -63,16 +64,16 @@ the things I can help you with.
 *Main* commands available:
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
+ • /donate: information on how to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
-
-
 {}
 And the following:
 """.format(
-    dispatcher.bot.first_name, ""
-    if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+    dispatcher.bot.first_name,
+    "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
+)
 
 BCB_IMG = "https://telegra.ph/file/5361963f60fd1307ccd8b.jpg"
 
