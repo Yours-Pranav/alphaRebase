c970599f9e4d07f5bc92efdd81a7bfb0ca0ca350
Billy <72440832+billybot482@users.noreply.github.com>
2023-01-09 20:31:51 +0800

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -568,5 +568,5 @@ def main():
 
 if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    telethn.start(bot_token=TOKEN)
+    #telethn.start(bot_token=TOKEN)
     main()
